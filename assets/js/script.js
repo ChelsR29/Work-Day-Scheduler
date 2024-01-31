5817234017bf4ffdbc4230f1918a96901569a311
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-01-30 21:42:06 -0500

    Updated starter code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,51 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+$(document).ready(function() {
+
+  // Get current hour
+  var currentHour = dayjs().hour();
+
+  // Loop through each time block
+  $(".time-block").each(function() {
+    // Extract the hour from the id
+    var blockHour = parseInt($(this).attr("id").split("-")[1]);
+
+    // Add or remove classes based on the comparison
+    if (blockHour < currentHour) {
+      $(this).addClass("past");
+    } else if (blockHour === currentHour) {
+      $(this).addClass("present");
+    } else {
+      $(this).addClass("future");
+    }
+  });
+
+  // Load saved data from local storage
+  $(".time-block").each(function() {
+    var blockId = $(this).attr("id");
+    var savedData = localStorage.getItem(blockId);
+
+    if (savedData) {
+      $(this).find(".description").val(savedData);
+    }
+  });
+
+  $(".saveBtn").on("click", function() {
+    // Find the parent ".time-block" element
+    var timeBlock = $(this).parent(".time-block");
+    
+    // Retrieve the id attribute and user input
+    var blockId = timeBlock.attr("id");
+    var inputData = timeBlock.find(".description").val();
+    
+    // Save data to local storage
+    localStorage.setItem(blockId, inputData);
+    $("#local-storage").text("Appointment added to local storage");
+  });
+
+    // Display current date at the top
+    var currentDate = dayjs().format("dddd, MMMM D");
+    $("#currentDay").text(currentDate);
+
 });
